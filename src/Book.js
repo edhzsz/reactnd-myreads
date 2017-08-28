988a6fb19d20fcb07e9d7f6db24e849945098791
Edgar Hernandez <edhzsz@gmail.com>
2017-08-28 14:55:32 +0200

    code: Convert Book to a stateless component

--- src/Book.js
+++ src/Book.js
@@ -1,21 +1,15 @@
-import React, { Component } from 'react'
+import React from 'react'
 import BookShelfChanger from './BookShelfChanger.js'
 
-class Book extends Component {
-  render() {
-    const { name, authors, coverWidth, coverHeight, coverUrl} = this.props
-
-    return (
-      <div className="book">
-        <div className="book-top">
-          <div className="book-cover" style={{ width: coverWidth, height: coverHeight, backgroundImage: `url("${coverUrl}")` }}></div>
-            <BookShelfChanger />
-          </div>
-        <div className="book-title">{name}</div>
-        <div className="book-authors">{authors}</div>
-      </div>
-    );
-  }
-}
+let Book = ({ name, authors, coverWidth, coverHeight, coverUrl}) => (
+    <div className="book">
+      <div className="book-top">
+        <div className="book-cover" style={{ width: coverWidth, height: coverHeight, backgroundImage: `url("${coverUrl}")` }}></div>
+          <BookShelfChanger />
+        </div>
+      <div className="book-title">{name}</div>
+      <div className="book-authors">{authors}</div>
+    </div>
+  );
 
 export default Book;
