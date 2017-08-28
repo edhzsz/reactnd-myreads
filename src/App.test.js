13a7f30f1bd45077fc897da3d445bbf556ff4dff
Edgar Hernandez <edhzsz@gmail.com>
2017-08-28 16:07:37 +0200

    code: Add empty snapshots for the app

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import renderer from 'react-test-renderer';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+describe('Component: App', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+  });
+
+  it('should match its empty snapshot', () => {
+    const tree = renderer.create(
+      <App />
+     ).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
 });
