844a0d7aefaf0419f9990048afa73a4a197b2b18
Edgar Hernandez <edhzsz@gmail.com>
2017-08-29 11:06:19 +0200

    code: Render the books from props in the BooksGrid component. Adjusted tests and make the rendering shallow also. Adjusted snapshots of all other components tests. Rendering start to become dynamic.

--- src/BooksGrid.test.js
+++ src/BooksGrid.test.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import BooksGrid from './BooksGrid.js';
 import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
 
 describe('Component: BooksGrid', () => {
   it('renders without crashing', () => {
@@ -10,9 +12,34 @@ describe('Component: BooksGrid', () => {
   });
 
   it('should match its empty snapshot', () => {
-    const tree = renderer.create(
+    const tree = toJson(shallow(
       <BooksGrid />
-     ).toJSON();
+    ));
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should render the books array', () => {
+    const books = [
+      {
+        title: "To Kill a Mockingbird",
+        authors: ["Harper Lee"],
+        coverUrl: "http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api",
+        shelf: "currentlyReading",
+        id: "nggnmAEACAAJ"
+      },
+      {
+        title: "Ender's Game",
+        authors: ["Orson Scott Card"],
+        coverUrl: "http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api",
+        shelf: "currentlyReading",
+        id: "sJf1vQAACAAJ"
+      }
+    ];
+
+    const tree = toJson(shallow(
+      <BooksGrid books={books}/>
+    ));
 
     expect(tree).toMatchSnapshot();
   });
