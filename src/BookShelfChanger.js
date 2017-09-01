34d97630291b5a36a9861d8c72e851af6d34b291
Edgar Hernandez <edhzsz@gmail.com>
2017-09-01 22:07:11 +0200

    code: hide the none option when there is no shelf selected

--- src/BookShelfChanger.js
+++ src/BookShelfChanger.js
@@ -1,15 +1,15 @@
 import React from 'react'
 
-const BookShelfChanger = ({ book, onShelfChange }) => { 
-  return book 
+const BookShelfChanger = ({ book, onShelfChange }) => {
+  return book
   ? (
     <div className="book-shelf-changer">
-      <select value={book.shelf} onChange={(event) => onShelfChange(book, event.target.value)}>
+      <select value={book.shelf || 'none'} onChange={(event) => onShelfChange(book, event.target.value)}>
         <option value="none" disabled>Move to...</option>
         <option value="currentlyReading">Currently Reading</option>
         <option value="wantToRead">Want to Read</option>
         <option value="read">Read</option>
-        <option value="none">None</option>
+        {book.shelf && <option value="none">None</option>}
       </select>
     </div>
   ) : null;
--- src/BookShelfChanger.test.js
+++ src/BookShelfChanger.test.js
@@ -36,5 +36,23 @@ describe('Component: BookShelfChanger', () => {
     expect(tree).toMatchSnapshot();
   });
 
+  it('should not show non when there is no shelf selected', () => {
+    const book = {
+      title: "Harry Potter and the Sorcerer's Stone",
+      authors: ["J.K. Rowling"],
+      imageLinks: {
+        smallThumbnail:"http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=5&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api",
+        thumbnail:"http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api",
+      },
+      id: "wrOQLV6xB-wC"
+    };
+
+    const tree = renderer.create(
+      <BookShelfChanger book={book} onShelfChange={() => true} />
+     ).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
   // todo: test the onChange event, it should call the handler with correct parameters
 });
