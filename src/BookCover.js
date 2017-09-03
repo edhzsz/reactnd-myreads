45f7b13c3292c43fd7504455eaee10af3efa24c1
Edgar Hernandez <edhzsz@gmail.com>
2017-09-03 19:48:38 +0200

    refactor: Correct the style of the book covers.

--- src/BookCover.js
+++ src/BookCover.js
@@ -2,14 +2,11 @@ import React from 'react'
 
 const BookCover = ({ imageLinks }) => {
   const url = imageLinks && (imageLinks.thumbnail || imageLinks.smallThumbnail);
+  const style = url && { backgroundImage: `url("${url}")` };
 
-  const style = url
-    ? { width: 128, height: 192, backgroundImage: `url("${url}")` }
-    : { width: 128, height: 192 };
-    
   return (
     <div className="book-cover" style={style}></div>
   );
 }
 
-export default BookCover
+export default BookCover;
